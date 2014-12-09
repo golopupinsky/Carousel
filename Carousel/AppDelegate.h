38e26ee4fc79105173a07ea8a32b398759e3be50
Sergey Yuzepovich <sergeyyuzepovich@MacBook-Air-Sergey.local>
2014-12-09 16:11:52 +0300

    Initial Commit

---
+++ Carousel/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Carousel
+//
+//  Created by Sergey Yuzepovich on 09.12.14.
+//  Copyright (c) 2014 Sergey Yuzepovich. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Carousel/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Carousel
+//
+//  Created by Sergey Yuzepovich on 09.12.14.
+//  Copyright (c) 2014 Sergey Yuzepovich. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
